38c91b23e5fcfbc0f58785483e690a1e6d871736
Djonathan Quadras <djonquadras@gmail.com>
2022-01-18 14:04:21 -0300

    Aulas.

--- Python/coding.py
+++ Python/coding.py
@@ -1,12 +1,6 @@
-firstName = input("First Name = ").lower()
-secondName = input("Second Name = ").lower()
-decimal = 0
-unidade = 0
-for i in "true":
-    decimal += firstName.count(i)
-    decimal += secondName.count(i)
-for i in "love":
-    unidade += firstName.count(i)
-    unidade += secondName.count(i)
-    
-print(f"Total = {decimal}{unidade}%")
+import random
+
+coisas = ["tapete", "colher", "pneu", "morcego"]
+print(coisas)
+random.shuffle(coisas)
+print(coisas)
